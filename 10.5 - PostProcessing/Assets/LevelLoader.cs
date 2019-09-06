e016af47c7cc445ec3b67af0d3afe803eb98cbe6
A7ocin <alocin.uarag@gmail.com>
2019-09-06 11:46:04 +0200

    Addded postprocessing scene

---
+++ 10.5 - PostProcessing/Assets/LevelLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator anim;
+
+    public void LoadLevel(int buildIndex)
+    {
+        anim.SetTrigger("ChangeScene");
+        StartCoroutine(Wait(1.5f, buildIndex));
+    }
+
+    IEnumerator Wait(float time, int index)
+    {
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ 10.5 - PostProcessing/Assets/Scripts/KeyPadManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyPadManager : MonoBehaviour
+{
+    public TextMeshProUGUI textLabel;
+    public GameObject player;
+    public Camera keypadCam;
+
+    private bool focusOnKeypad;
+    private bool canSwitch = true;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            textLabel.text = "Press E";
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            textLabel.text = "";
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && other.gameObject.CompareTag("Player") && canSwitch && !focusOnKeypad)
+        {
+            player.GetComponent<PlayerMovement>().enabled = false;
+            player.GetComponent<Rigidbody>().velocity = Vector3.zero; // To avoid exiting from the trigger due to the accumulated forces 
+            player.GetComponent<PlayerMovement>().cam.GetComponent<Camera>().enabled = false;
+
+            keypadCam.enabled = true;
+
+            textLabel.text = "";
+
+            focusOnKeypad = true;
+            StartCoroutine(WaitForSwitch(0.1f));
+        }
+        else if ((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Escape)) && other.gameObject.CompareTag("Player") && canSwitch && focusOnKeypad)
+        {
+            player.GetComponent<PlayerMovement>().enabled = true;
+            player.GetComponent<PlayerMovement>().cam.GetComponent<Camera>().enabled = true;
+
+            keypadCam.enabled = false;
+
+            textLabel.text = "Press E";
+
+            focusOnKeypad = false;
+
+            StartCoroutine(WaitForSwitch(0.1f));
+        }
+    }
+
+    IEnumerator WaitForSwitch(float seconds)
+    {
+        canSwitch = false;
+
+        yield return new WaitForSeconds(seconds);
+
+        canSwitch = true;
+    }
+}
